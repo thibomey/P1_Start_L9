806c6284e551e7362e0b1dc1df13644e9acd0f31
Tessa <48088982+Tesax123@users.noreply.github.com>
2024-06-08 13:14:38 +0200

    Update main.py DISPLAYSURF -> venster (#1)

--- startcode/main.py
+++ startcode/main.py
@@ -2,7 +2,7 @@ import pygame, sys
 from pygame.locals import QUIT
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((200, 50))
+venster = pygame.display.set_mode((200, 50))
 
 while True:
     for event in pygame.event.get():
